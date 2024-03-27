cc5cfdaa982fc881e80a47b4ec6025bd905bf55d
YashsviG <girdhar.yashsvi@gmail.com>
2024-03-27 13:48:52 -0700

    cleaned up the file, added argParser

--- dataPrep.py
+++ dataPrep.py
@@ -1,29 +1,71 @@
+import argparse
 import json
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 keys_to_remove = ["review_id", "business_id", "user_id", "date"]
-outfile = './data.json'
+outfile = "./data2.json"
+input_filename = "./test.json"
+punctuation = "\"#$%&'()*+,-./:;<=>@[\]^_`{|}~.\n"
+replacement_mapping = {"&": "and"}
 
-with open("./test.json", "r") as file, open(outfile, "a") as output_file:
-    for line in file:
-        data = json.loads(line)
-        for key in keys_to_remove:
-            data.pop(key)
-        output_file.write(json.dumps(data, ensure_ascii=False)+'\n')
-    output_file.close()
 
-file.close()
+# Remove punctuations from text
+def remove_punctuation(line):
+    translation_table = str.maketrans("", "", punctuation)
 
-df = pd.read_json(outfile, lines=True)
-train_ratio = 0.8
-test_ratio = 0.10
-val_ratio = 0.10
-test_val_ratio = test_ratio / (test_ratio + val_ratio)
+    for char, replacement in replacement_mapping.items():
+        translation_table[ord(char)] = replacement
 
-train_data, remaining_data = train_test_split(df, test_size=(1-train_ratio))
-test_data, val_data = train_test_split(remaining_data, test_size=test_val_ratio)
+    line["text"] = line["text"].translate(translation_table)
 
-train_data.to_json("train_data.json", orient="records", lines=True)
-test_data.to_json("test_data.json", orient="records", lines=True)
-val_data.to_json("val_data.json", orient="records", lines=True)
+
+def load_raw_json(input_filename):
+    with open(input_filename, "r") as file, open(outfile, "a") as output_file:
+        for line in file:
+            data = json.loads(line)
+            for key in keys_to_remove:
+                data.pop(key)
+            remove_punctuation(data)
+            output_file.write(json.dumps(data, ensure_ascii=False) + "\n")
+        output_file.close()
+    file.close()
+
+
+def split_data(output_file=outfile):
+    df = pd.read_json(output_file, lines=True)
+    train_ratio = 0.8
+    test_ratio = 0.10
+    val_ratio = 0.10
+    test_val_ratio = test_ratio / (test_ratio + val_ratio)
+
+    train_data, remaining_data = train_test_split(df, test_size=(1 - train_ratio))
+    test_data, val_data = train_test_split(remaining_data, test_size=test_val_ratio)
+
+    train_data.to_json("train_data.json", orient="records", lines=True)
+    test_data.to_json("test_data.json", orient="records", lines=True)
+    val_data.to_json("val_data.json", orient="records", lines=True)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Data Prep Script")
+
+    parser.add_argument("--input_raw_file", type=str, help="Raw json input filepath")
+    parser.add_argument("--input_file", type=str, help="Process json input filepath")
+    parser.add_argument("--split-data", action="store_true", help="Flag to split data")
+
+    args = parser.parse_args()
+
+    if args.split_data:
+        if not args.input_raw_file:
+            print("No Input file provided to split, using default values")
+        split_data()
+    elif args.input_raw_file:
+        load_raw_json(args.input_raw_file)
+    else:
+        print("No action specified. Use --split-data or provide --input_raw_file")
+
+
+if __name__ == "__main__":
+    main()
