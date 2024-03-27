1f7e109eae424ce1d9f887db3b22741500da3a24
YashsviG <girdhar.yashsvi@gmail.com>
2024-03-27 12:39:34 -0700

    able to clean the useless fields

--- dataPrep.py
+++ dataPrep.py
@@ -1,12 +1,14 @@
 import json
 import io
 
-with open("./yelp_academic_dataset_review.json", "r") as file:
-    data = json.load(file)
-    keys_to_remove = ["review_id", "business_id", "user_id", "date"]
-    for key in keys_to_remove:
-        data.pop(key)
+keys_to_remove = ["review_id", "business_id", "user_id", "date"]
+outfile = './data.json'
 
-    outfile = io.open('data.json','a')
-
-    json.dump(data, fp= outfile, indent=4)
+with open("./test.json", "r") as file, open(outfile, "a") as output_file:
+    for line in file:
+        data = json.loads(line)
+        for key in keys_to_remove:
+            data.pop(key)
+        output_file.write(json.dumps(data)+'\n')
+    output_file.close()
+    
