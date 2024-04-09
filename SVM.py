b83059888f7e32e633416ed9f824b1df15018aeb
YashsviG <girdhar.yashsvi@gmail.com>
2024-04-08 22:55:19 -0700

    wip - svm

--- SVM.py
+++ SVM.py
@@ -3,16 +3,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
-from sklearn.metrics import (
-    classification_report,
-    recall_score,
-    precision_score,
-    f1_score,
-    roc_curve,
-    auc,
-    precision_recall_curve,
-)
+from sklearn.metrics import classification_report
 import json
+from sklearn.feature_extraction.text import CountVectorizer
+import pickle
 
 names = ["SVM"]
 
@@ -43,21 +37,13 @@ yelp_class['stars'].value_counts()
 yelp_class.info()
 
 X = yelp_class['text']
+vectorizer = CountVectorizer(stop_words="english")
+X = vectorizer.fit_transform(yelp_class['text'])
+
 y = yelp_class['stars']
-y2 = yelp_class['useful']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+clf_stars = SVC(kernel="linear", random_state=42)
+clf_stars.fit(X, y)
 
-clf_pipe.fit(X_train, y_train)
-
-pred = clf_pipe.predict(X_test)
-pred_prob = clf_pipe.predict_proba(X_test)[:, 1]
-
-# fpr, tpr, thresholds = roc_curve(y_test, pred_prob)
-# precision, recall, thresholds_pr = precision_recall_curve(y_test, pred)
-
-print("\n\n", name, "\n\n")
-print(classification_report(y_test, pred))
-# print("ROC AUC: ", auc(fpr, tpr))
-# print("Precision/Recall AUC: ", auc(precision, recall))
-print("\n\n")
+with open(f"svm.pickle", "wb") as f:
+        pickle.dump((clf_stars, vectorizer), f)
