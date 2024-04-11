a8e7d300ad549cbebdac9e7640cc2fb3956cc177
oghodwhy <moena94@hotmail.com>
2024-04-10 23:00:36 -0700

    Added timing metrics and Test results into train_model and run_test.

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -6,12 +6,14 @@ import argparse
 import os
 import sys
 import pandas as pd
+import time
+import datetime
 from sklearn.feature_extraction.text import CountVectorizer
 
 DEFAULT_TEST_FILE = 'data/test_data.json'
 DEFAULT_TRAIN_FILE = 'data/train_data.json'
 DEFAULT_MODEL_FILE = 'naive_bayes.pkl'
-CHUNK_SIZE = 1000
+CHUNK_SIZE = 10000
 
 EMPTY_FEATURE_VECTOR = dict({'useful': [0, 0], 'funny': [0, 0], 'cool': [0, 0]})
 
@@ -123,14 +125,14 @@ class NaiveBayes(object):
         })
 
     def train_classifier(self):
-        print('training classifier...')
+        print('Training classifier...')
+        train_start = datetime.datetime.now()
 
         total_null_samples = 0
         total_samples = 0
 
         self.p_stars = ProbDist.ProbDist('stars',
-                                         freqs={1: 1, 2: 1, 3: 1, 4: 1,
-                                                5: 1})  # Default freq set to 1 to avoid zero inputs
+                                         freqs={1: 1, 2: 1, 3: 1, 4: 1, 5: 1})  # Default set to 1 to avoid zero inputs
         self.p_has_word_category = ProbDist.JointProbDist(['hasWord', 'stars'])
 
         self.feature_vectors_dict: dict[str: dict[str: [float, int]]] = dict(
@@ -143,6 +145,7 @@ class NaiveBayes(object):
         for chunk in df:
             print('Training with chunk of ' + str(chunk.shape[0]) + ' samples...')
             null_samples = 0
+            start = time.time()
 
             # Handle line probabilities
             for i, line in chunk.iterrows():
@@ -158,34 +161,40 @@ class NaiveBayes(object):
                     self.p_stars[lineStars] += 1
 
                     vectorizer = CountVectorizer(stop_words='english')
-                    vectorizer.fit([line['text']])
+                    try:
+                        vectorizer.fit([line['text']])
 
-                    total_words = float(sum(vectorizer.vocabulary_.values()))
+                        total_words = float(sum(vectorizer.vocabulary_.values()))
 
-                    # Handle word probabilities within line
-                    for word in vectorizer.vocabulary_:
-                        self.p_has_word_category[word, lineStars] += vectorizer.vocabulary_[word]
+                        # Handle word probabilities within line
+                        for word in vectorizer.vocabulary_:
+                            self.p_has_word_category[word, lineStars] += vectorizer.vocabulary_[word]
 
-                    # if we're doing feature regression, add feature contributions to words feature vector in dict.
-                    if self.incFeatures:
-                        for word in vectorizer.vocabulary_.keys():
+                        # if we're doing feature regression, add feature contributions to words feature vector in dict.
+                        if self.incFeatures:
+                            for word in vectorizer.vocabulary_.keys():
 
-                            # check we have feature vector for word, create one if needed
-                            self.feature_vectors_dict[word] = self.feature_vectors_dict.get(word, EMPTY_FEATURE_VECTOR)
+                                # check we have feature vector for word, create one if needed
+                                self.feature_vectors_dict[word] = self.feature_vectors_dict.get(word,
+                                                                                                EMPTY_FEATURE_VECTOR)
 
-                            # Add word component strengths to vector components, increment occurrences.
-                            if lineUseful > 0:
-                                self.feature_vectors_dict[word]['useful'][0] += lineUseful * vectorizer.vocabulary_[
-                                    word] / max(total_words, 1.0)
-                                self.feature_vectors_dict[word]['useful'][1] += vectorizer.vocabulary_[word]
-                            if lineFunny > 0:
-                                self.feature_vectors_dict[word]['funny'][0] += lineFunny * vectorizer.vocabulary_[
-                                    word] / max(total_words, 1.0)
-                                self.feature_vectors_dict[word]['funny'][1] += vectorizer.vocabulary_[word]
-                            if lineCool > 0:
-                                self.feature_vectors_dict[word]['cool'][0] += lineCool * vectorizer.vocabulary_[
-                                    word] / max(total_words, 1.0)
-                                self.feature_vectors_dict[word]['cool'][1] += vectorizer.vocabulary_[word]
+                                # Add word component strengths to vector components, increment occurrences.
+                                if lineUseful > 0:
+                                    self.feature_vectors_dict[word]['useful'][0] += lineUseful * vectorizer.vocabulary_[
+                                        word] / max(total_words, 1.0)
+                                    self.feature_vectors_dict[word]['useful'][1] += vectorizer.vocabulary_[word]
+                                if lineFunny > 0:
+                                    self.feature_vectors_dict[word]['funny'][0] += lineFunny * vectorizer.vocabulary_[
+                                        word] / max(total_words, 1.0)
+                                    self.feature_vectors_dict[word]['funny'][1] += vectorizer.vocabulary_[word]
+                                if lineCool > 0:
+                                    self.feature_vectors_dict[word]['cool'][0] += lineCool * vectorizer.vocabulary_[
+                                        word] / max(total_words, 1.0)
+                                    self.feature_vectors_dict[word]['cool'][1] += vectorizer.vocabulary_[word]
+
+                    # Handling for text that is all stopwords, such as "a a a a a" or "x"
+                    except ValueError:
+                        null_samples += 1
 
                 # Else handle misshapen lines
                 else:
@@ -194,10 +203,17 @@ class NaiveBayes(object):
                 total_samples += 1
 
             self.total_samples = total_samples
+            time_taken = time.time() - start
 
-            print('Done chunk. Found ' + str(null_samples) + ' null samples.')
+            print('Completed chunk up to {} in {:.2f} sec. \nFound {} null entries in chunk'
+                  .format(total_samples, time_taken, total_null_samples))
 
+        train_time = datetime.datetime.now() - train_start
+
+        print('Completed training on {} items in {}.'.format(total_samples, train_time))
         print('Normalizing probability matrix...')
+
+        Normal_start = datetime.datetime.now()
 
         # Iterate over ratings and normalize probabilities.
         for rating in self.p_has_word_category.values('stars'):
@@ -206,7 +222,9 @@ class NaiveBayes(object):
             for word in self.p_has_word_category.values('hasWord'):
                 self.p_has_word_category[word, rating] = self.p_has_word_category[word, rating] / float(full_count)
 
-        print('Done')
+        normal_time = datetime.datetime.now() - Normal_start
+        print('Normalization completed in {}.'.format(normal_time))
+        print('Done Training.')
 
     def load_model(self):
         print('THIS IS NOT IMPLEMENTED YET, ABORTING')
@@ -225,6 +243,7 @@ class NaiveBayes(object):
         zero_prob = 1.0 / float(self.total_samples)
 
         print('Running predictions on test file at ' + self.testFile + ' ...')
+        test_start = datetime.datetime.now()
 
         # Load chunk from testfile with pandas
         df = pd.read_json(self.testFile, lines=True, chunksize=CHUNK_SIZE)
@@ -232,6 +251,7 @@ class NaiveBayes(object):
         # For each chunk of test data, categorize and compare results
         for chunk in df:
             print('Testing with chunk of ' + str(chunk.shape[0]) + ' samples...')
+            chunk_start = time.time()
 
             # For Each line in chunk, run classification and regression
             for line_index, line in chunk.iterrows():
@@ -319,6 +339,22 @@ class NaiveBayes(object):
 
                 # Increment test total for completed line
                 test_total += 1
+
+            chunk_time = time.time() - chunk_start
+            print('Completed prediction on {} samples in {:.2f} sec.'.format(test_total, chunk_time))
+
+        # Calculate test statistics
+        test_time = datetime.datetime.now() - test_start
+
+        accuracy = test_correct / test_total * 100.00
+
+        print('Completed prediction on {} samples in {}.'.format(test_total, test_time))
+        print('Found {} null entries in test file.'.format(test_null))
+        print('Rating Accuracy:     {:.2f} %'.format(accuracy))
+
+        if self.incFeatures:
+            print('Feature MAE:         {:.4f}', format(round(residual_absolute_total / test_total)))
+            print('Feature MSE:         {:.4f}', format(round(residual_squared_total / test_total)))
 
 
 def main():
